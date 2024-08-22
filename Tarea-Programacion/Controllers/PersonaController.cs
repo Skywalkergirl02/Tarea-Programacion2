2a1641b2ecd4eed208992aa2d73b01aa1bb078f0
macar <macar@DESKTOP-BP907I0>
2024-08-22 11:49:50 -0600

    Agregar archivos de proyecto.

---
+++ Tarea-Programacion/Controllers/PersonaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tarea_Programacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+        // POST api/<PersonaController>
+        [HttpPost]
+        public List <Persona> GetPersonas(Persona persona)
+        {
+            List<Persona> lista= new List<Persona>();
+            for (int i = 0;i<10;i++)
+            {
+                lista.Add(persona);
+            }
+            return lista;
+        }
+    }
+}
+
+        
---
+++ Tarea-Programacion/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarea_Programacion
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad {  get; set; }
+        public string profesion {  get; set; }
+    }
+}
